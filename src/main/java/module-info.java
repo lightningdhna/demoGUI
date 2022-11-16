d2c6c01c992379098a3eb73fd1de2121d05a2f8b
hung <shalightdowdh@gmail.com>
2022-11-16 22:05:29 +0700

    :) 16/11 22:05 no idea

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,19 @@
 module com.example.demogui {
     requires javafx.controls;
     requires javafx.fxml;
-        requires javafx.web;
-            
-        requires org.controlsfx.controls;
-            requires com.dlsc.formsfx;
-            requires validatorfx;
-            requires org.kordamp.ikonli.javafx;
-            requires org.kordamp.bootstrapfx.core;
-            requires eu.hansolo.tilesfx;
-            requires com.almasb.fxgl.all;
-    
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
     opens com.example.demogui to javafx.fxml;
     exports com.example.demogui;
+
+    opens dhung to javafx.fxml;
+    exports dhung;
 }
